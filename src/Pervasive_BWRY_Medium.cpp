a1ec198000204a7803e8d41b298fd88b512cbe5c
arvin-tan <arvin_tan@pervasivedisplays.com>
2026-02-25 11:09:51 +0800

    Update Pervasive_BWRY_Medium.cpp

--- src/Pervasive_BWRY_Medium.cpp
+++ src/Pervasive_BWRY_Medium.cpp
@@ -183,7 +183,7 @@ void Pervasive_BWRY_Medium::COG_getDataOTP()
 
 void Pervasive_BWRY_Medium::COG_initial()
 {
-    b_sendCommandData8(0xe6, 0x19); //	25 C temp
+    b_sendCommandData8(0xe6, u_temperature); //	25 C temp
     b_sendCommandData8(0xe0, 0x02); //
 
     switch (u_eScreen_EPD)
